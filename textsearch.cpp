65a6d893ef0dd049a010def1716ff820417c2e83
John Mauney <parkermauney+git@gmail.com>
2015-04-06 17:06:48 -0400

    Text search now working correctly

--- textsearch.cpp
+++ textsearch.cpp
@@ -7,10 +7,9 @@
  * strings containing the input as a substring.
  * 
  * Created on November 06, 2014
- * Last modified November 11, 2014
+ * Last modified April 6, 2015
  */
  
-#include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -22,8 +21,8 @@ int main(int argc, char** argv) {
     int numStates = 0;
     string input;
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    int inputLength;
     int alphabetLength = alphabet.length();
+    int inputLength;
     int acceptingState;
     int offset = 97; //char to int offset for lowercase letters
      
@@ -42,33 +41,27 @@ int main(int argc, char** argv) {
     */
     //create array for transition table
     int trans[numStates][alphabetLength]; 
-    // for (int i = 0; i < numStates; i++) {
-    //     for (int j = 0; j < alphabetLength; j++) {
-    //         trans[i][j] = 0;
-    //     }
-    // }
 
-    //compute transition table via Knuth-Morris-Pratt construction
-    //this section accounts for finding a pure substring without looping
-    for (int i = 0; i < inputLength; i++) {
-        trans[i][input.at(i) - offset] = i + 1;
-        //fill the rest of state 1 with 0's
-        if (i == 0) {
-            for (int j = 0; j < alphabetLength; j++) {
-                if (j != input.at(i) - offset)
-                    trans[i][j] = 0;
-            }
-        }
+    //Compute transition table via Knuth-Morris-Pratt construction.
+    //Process state 1 on its own so we can fill the rest of it with 0's.
+    trans[0][input.at(0) - offset] = 1;
+    for (int i = 0; i < alphabetLength; i++) {
+        if (i != input.at(0) - offset)
+            trans[0][i] = 0;
     }
 
-    //now we account for loops within the substring
-    int s = 0;
-    for (int i = 0; i < inputLength; i++) {
+    //First copy the mismatch case to account for loops within the 
+    //substring. Then set the match case to account for finding the
+    //pure string without looping. Finally save the restart state
+    //for the mismatch case.
+    int s = 0; //restart state for mismatch case
+    for (int i = 1; i < inputLength; i++) {
         for (int j = 0; j < alphabetLength; j++) {
             if (j != input.at(i) - offset)
-                trans[i][j] = trans[s][j];
+                trans[i][j] = trans[s][j]; //mismatch case
         }
-        s = trans[s][input.at(i) - offset];
+        trans[i][input.at(i) - offset] = i + 1; //match case
+        s = trans[s][input.at(i) - offset]; //set restart state
     }
 
     //the last state will only transition to itself
@@ -82,7 +75,7 @@ int main(int argc, char** argv) {
     */
     cout << "Number of states: " << numStates << endl;
     cout << "Accepting states: " << acceptingState << endl;
-    cout << "Alphabet: abcdefghijklmnopqrstuvwxyz" << endl;
+    cout << "Alphabet: " << alphabet << endl;
     for (int i = 0; i < numStates; i++) {
         for (int j = 0; j < alphabetLength - 1; j++) {
             cout << trans[i][j] << " ";
